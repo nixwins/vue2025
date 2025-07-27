85022afe504d3427fc669f8ea33c0ae3377c1e7a
Dos <halaulilau@gmail.com>
2025-07-27 17:44:24 +0500

    Implement counter functionality and add Todos component with todo management features

--- my-first-app/src/App.vue
+++ my-first-app/src/App.vue
@@ -1,37 +1,54 @@
 <script setup lang="js">
+import Todos from './Todos.vue';
 import { ref } from 'vue';
 
+const count = ref(0)
+console.log(count)
 
-const name = ref('')
-const changeName = function (event) {
-  console.log("event", event);
-  const inputValue = event.target.value;
-  name.value = inputValue;
-};
+const increment = function() {
+  console.log('increment')
+  count.value = count.value + 1
+}
 
-
-
+const imgSrc = ref('https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/730/capsule_616x353.jpg?t=1749053861')
 const users = ref([
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Jim" },
-]);
-
-const addNewUser = function () {
-  users.value.push({name:name.value, id:new Date().getTime()})
-};
+  {
+    id: 1,
+    name: 'John',
+    age: 20
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    age: 21
+  },
+  {
+    id: 3,
+    name: 'Jim',
+    age: 22
+  }
+])
 </script>
 <template>
-  <div>
-    <!-- <p :id="counter">Counter: {{ counter }}</p>
-    <h1>Hello {{ name }}</h1>
-    <button @click="incCounter">Click me</button> -->
-    <input type="text" v-bind:value="name" @input="changeName" />
-    <button @click="addNewUser">Add new user</button>
-    <p v-for="user in users" :key="user.id">
-      {{ user.name }}
-      {{ user.id }}
-    </p>
+  <div class="container">
+    <Todos />
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} - {{ user.age }}
+      </li>
+    </ul>
+
+    <p id="counter">Счётчик: {{ count }}</p>
+    <button id="incrementBtn" @click="increment">Увеличить</button>
+    <h1 :id="count">Hello World</h1>
+    <img v-bind:src="imgSrc" />
   </div>
 </template>
-<style></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ my-first-app/src/Todos.vue
@@ -0,0 +1,33 @@
+<script setup lang="js">
+import { ref } from 'vue';
+
+const todos = ref([
+  {
+    id: 1,
+    name: 'Reading',
+  },
+])
+
+const todoName = ref('')
+function handleTodoName(event) {
+  console.log("123", event)
+  todoName.value = event.target.value
+}
+
+function addNewTodo() {
+   todos.value.push({id:Math.random(), name: todoName.value})
+}
+</script>
+<template>
+  <div>
+    <h1>{{ todoName }}</h1>
+    <input type="text" @input="handleTodoName" />
+    <button @click="addNewTodo">Add todo</button>
+    <ul>
+      <li v-for="item in todos" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped lang="scss"></style>
